28e08a5079fff38565c3a46a2d94b9f7b63a50cb
Chito Wong <chi.to.wong@outlook.com>
2022-04-30 15:17:38 +0800

    #1 Initialise project with package publishing

---
+++ package/builder/prebuild.ts
@@ -0,0 +1,62 @@
+// node --loader ts-node/esm .\builder\build.ts
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {readFile, mkdir, writeFile, copyFile} from 'fs/promises';
+import {CompanyEntry, TemplateEntry} from "../src";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const sourcePath = path.join(__dirname, '../../public/resources/templates');
+const targetPath = path.join(__dirname, '../src/templates');
+
+const copyCompanyIdList = async () => {
+    // read source file
+    const companyConfig = await readFile(path.join(sourcePath, 'company-config.json'), 'utf-8');
+
+    // copy to target dir
+    await mkdir(targetPath, {recursive: true});
+    await writeFile(path.join(targetPath, 'company-config.json'), companyConfig);
+};
+
+const createTemplateConfigsFile = async (): Promise<Record<string, TemplateEntry[]>> => {
+    // read company config
+    const companyConfigStr = await readFile(path.join(sourcePath, 'company-config.json'), 'utf-8');
+    const companyConfig = JSON.parse(companyConfigStr) as CompanyEntry[];
+
+    // read config source file for each company
+    const templateConfigs: Record<string, TemplateEntry[]> = {};
+    for (let company of companyConfig) {
+        const companyId = company.id;
+        const configStr = await readFile(path.join(sourcePath, companyId, '_config.json'), 'utf-8');
+        templateConfigs[companyId] = JSON.parse(configStr) as TemplateEntry[];
+    }
+
+    // write template configs
+    await mkdir(targetPath, {recursive: true});
+    await writeFile(path.join(targetPath, 'template-configs.json'), JSON.stringify(templateConfigs));
+
+    return templateConfigs;
+}
+
+const copyTemplate = async (companyId: string, filename: string) => {
+    // read source file
+    const templateStr = await readFile(path.join(sourcePath, companyId, filename + '.json'), 'utf-8');
+
+    // copy to target dir
+    await mkdir(path.join(targetPath, companyId), {recursive: true});
+    await writeFile(path.join(targetPath, companyId, filename + '.json'), templateStr);
+}
+
+const prebuild = async () => {
+    await copyCompanyIdList();
+    const templateConfigs = await createTemplateConfigsFile();
+
+    for (let [companyId, templateList] of Object.entries(templateConfigs)) {
+        for (let template of templateList) {
+            await copyTemplate(companyId, template.filename);
+        }
+    }
+}
+
+prebuild().then();
---
+++ package/src/index.ts
@@ -0,0 +1,44 @@
+import companyConfigJson from './templates/company-config.json';
+import templateConfigsJson from './templates/template-configs.json';
+
+export interface CompanyEntry {
+    id: string;
+    name: Translation;
+}
+
+export interface TemplateEntry {
+    filename: string;
+    name: Translation;
+}
+
+enum LanguageCode {
+    Azerbaijani = 'az',
+    Arabic = 'ar',
+    Catalan = 'ca',
+    Chinese = 'zh',
+    ChineseCN = 'zh-CN',
+    ChineseSimp = 'zh-Hans',
+    ChineseTrad = 'zh-Hant',
+    ChineseHK = 'zh-HK',
+    ChineseTW = 'zh-TW',
+    English = 'en',
+    French = 'fr',
+    Gaelic = 'ga',
+    German = 'de',
+    Hindi = 'hi',
+    Japanese = 'ja',
+    Korean = 'ko',
+    Malay = 'ms',
+    Norwegian = 'no',
+    Spanish = 'es',
+    Persian = 'fa',
+    Portuguese = 'pt',
+    Russian = 'ru',
+    Swedish = 'sv',
+    Turkish = 'tr',
+}
+
+type Translation = { [l in LanguageCode]?: string };
+
+export const companyConfig = companyConfigJson as CompanyEntry[];
+export const templateList = templateConfigsJson as Record<string, TemplateEntry[]>;
