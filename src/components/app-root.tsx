eabc70a85c980d6f4293922dae17bdf2bc57aafc
Chito Wong <chi.to.wong@outlook.com>
2022-09-25 14:34:31 +0800

    #46 Make eslint happy

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,9 +1,9 @@
-import {Button, Flex, List, ListItem} from '@chakra-ui/react';
+import { Button, Flex, List, ListItem } from '@chakra-ui/react';
 import React from 'react';
 import WindowHeader from './window-header';
-import {useRootDispatch, useRootSelector} from '../redux';
-import {bumpCounter} from '../redux/app/app-slice';
-import {companyConfig, templateList} from '@railmapgen/rmg-templates-resources';
+import { useRootDispatch, useRootSelector } from '../redux';
+import { bumpCounter } from '../redux/app/app-slice';
+import { companyConfig, templateList } from '@railmapgen/rmg-templates-resources';
 
 export default function AppRoot() {
     const dispatch = useRootDispatch();
@@ -11,13 +11,13 @@ export default function AppRoot() {
 
     return (
         <Flex direction="column" height="100%" overflow="hidden">
-            <WindowHeader/>
+            <WindowHeader />
             This is a seed project for RMG with React framework.
-            <br/>
+            <br />
             Please replace any "RMG Templates" or "rmg-templates" with the correct component name.
-            <br/>
+            <br />
             Chakra UI and Redux store are setup already. Here's an example state: {counter}.
-            <br/>
+            <br />
             <Button onClick={() => dispatch(bumpCounter())}>Bump</Button>
             <List>
                 {companyConfig.map(company => (
@@ -25,8 +25,9 @@ export default function AppRoot() {
                         {company.name.en}
                         <List>
                             {templateList[company.id].map(template => (
-                                <ListItem key={template.filename}>{template.filename},
-                                    author: {template.updatedBy ?? 'unknown'}</ListItem>
+                                <ListItem key={template.filename}>
+                                    {template.filename}, author: {template.updatedBy ?? 'unknown'}
+                                </ListItem>
                             ))}
                         </List>
                     </ListItem>
