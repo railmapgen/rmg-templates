e34b3deffaabe6d3244fdf2468581fdea02354e2
Chito Wong <chi.to.wong@outlook.com>
2023-01-15 20:56:22 +0800

    #64 Replace jest with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+/// <reference types="vitest" />
+
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -8,6 +11,19 @@ export default defineConfig({
     plugins: [
         react(),
         checker({ typescript: true, eslint: { lintCommand: 'eslint ./src' } }),
+        legacy({
+            targets: ['defaults', '>0.2%', 'not dead'],
+            modernPolyfills: true,
+        }),
         splitVendorChunkPlugin(),
     ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        deps: {
+            fallbackCJS: true,
+        },
+        watch: false,
+    },
 });
