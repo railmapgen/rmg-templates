37ae19aabd23071b63d85ef529d0aaef7dae632d
Chito Wong <chi.to.wong@outlook.com>
2023-01-19 00:53:49 +0800

    #65 Fix issue bot scripts

--- package/issuebot/generate-review-url.js
+++ package/issuebot/generate-review-url.js
@@ -2,10 +2,12 @@ const gitHash = process.argv[2];
 const affectFilesArg = process.argv[3];
 const baseUrl = 'https://raw.githubusercontent.com/railmapgen/rmg-templates/';
 const templatesPath = '/public/resources/template/';
+const rmgUrl = 'https://uat-railmapgen.github.io/rmg/';
 const files = affectFilesArg.split(/[()]/)[1].split(',');
 
 console.log('REVIEW_URLS<<EOF');
 files.forEach(file => {
-    console.log(baseUrl + gitHash + templatesPath + file);
+    const externalUrl = baseUrl + gitHash + templatesPath + file;
+    console.log(`${rmgUrl}?external=${encodeURIComponent(externalUrl)}`);
 });
 console.log('EOF');
