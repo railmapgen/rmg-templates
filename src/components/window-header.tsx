378a87904da99b4a7959eb6fb88e35ed9323ea8c
Chito Wong <chi.to.wong@outlook.com>
2023-02-22 23:48:30 +0800

    #197 Support Korean translation

--- src/components/window-header.tsx
+++ src/components/window-header.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { Heading, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { RmgEnvBadge, RmgWindowHeader } from '@railmapgen/rmg-components';
-import { LanguageCode } from '@railmapgen/rmg-translate';
-import { handleLanguageChange } from '../i18n/config';
 import rmgRuntime from '@railmapgen/rmg-runtime';
-import { MdTranslate } from 'react-icons/md';
 
 export default function WindowHeader() {
     const { t } = useTranslation();
@@ -13,28 +9,12 @@ export default function WindowHeader() {
     const environment = rmgRuntime.getEnv();
     const appVersion = rmgRuntime.getAppVersion();
 
-    const handleSelectLanguage = (language: LanguageCode) => {
-        rmgRuntime.setLanguage(language);
-        handleLanguageChange(language);
-    };
-
     return (
         <RmgWindowHeader>
             <Heading as="h4" size="md">
                 {t('RMG Templates')}
             </Heading>
             <RmgEnvBadge environment={environment} version={appVersion} />
-
-            <HStack ml="auto">
-                <Menu>
-                    <MenuButton as={IconButton} icon={<MdTranslate />} variant="ghost" size="sm" />
-                    <MenuList>
-                        <MenuItem onClick={() => handleSelectLanguage(LanguageCode.English)}>English</MenuItem>
-                        <MenuItem onClick={() => handleSelectLanguage(LanguageCode.ChineseSimp)}>简体中文</MenuItem>
-                        <MenuItem onClick={() => handleSelectLanguage(LanguageCode.ChineseTrad)}>繁體中文</MenuItem>
-                    </MenuList>
-                </Menu>
-            </HStack>
         </RmgWindowHeader>
     );
 }
