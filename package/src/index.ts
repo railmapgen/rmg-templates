766d83b4fb989c9ab5eb806aadfd3f0ac31df98f
Chito Wong <chi.to.wong@outlook.com>
2023-03-02 17:09:54 +0800

    #197 Update language model with rmg-runtime and rmg-translate

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,6 @@
 import companyConfigJson from './templates/company-config.json';
 import templateConfigsJson from './templates/template-configs.json';
+import { Translation } from '@railmapgen/rmg-translate';
 
 export interface CompanyEntry {
     id: string;
@@ -18,35 +19,6 @@ export interface TemplateEntry {
     uploadBy?: string;
     style?: RmgStyle;
 }
-
-type LanguageCode =
-    | 'ar'
-    | 'az'
-    | 'ca'
-    | 'de'
-    | 'en'
-    | 'es'
-    | 'fa'
-    | 'fr'
-    | 'ga'
-    | 'gd'
-    | 'hi'
-    | 'ja'
-    | 'ko'
-    | 'ms'
-    | 'no'
-    | 'pt'
-    | 'ru'
-    | 'sv'
-    | 'tr'
-    | 'ur'
-    | 'zh'
-    | 'zh-CN'
-    | 'zh-Hans'
-    | 'zh-Hant'
-    | 'zh-HK'
-    | 'zh-TW';
-type Translation = Partial<Record<LanguageCode, string>>;
 
 export const coreCompanyConfig = companyConfigJson as CompanyEntry[];
 export const coreTemplateList = templateConfigsJson as Record<string, TemplateEntry[]>;
