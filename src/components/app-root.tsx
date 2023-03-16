d43e20624d0ec769b014147bd999d2784cb8cf88
Chito Wong <chi.to.wong@outlook.com>
2023-03-16 22:14:04 +0800

    #212 Listen to colour mode change

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,6 +1,6 @@
 import React, { lazy } from 'react';
 import WindowHeader from './window-header';
-import { RmgErrorBoundary, RmgLoader, RmgWindow } from '@railmapgen/rmg-components';
+import { RmgErrorBoundary, RmgLoader, RmgThemeProvider, RmgWindow } from '@railmapgen/rmg-components';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const TemplatesView = lazy(() => import('./templates-view/templates-view'));
@@ -9,27 +9,29 @@ const TicketView = lazy(() => import('./ticket-view/ticket-view'));
 export default function AppRoot() {
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <RmgWindow>
-                <WindowHeader />
-                <Routes>
-                    <Route
-                        path="/new"
-                        element={
-                            <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
-                                <TicketView />
-                            </RmgErrorBoundary>
-                        }
-                    />
-                    <Route
-                        path="/"
-                        element={
-                            <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
-                                <TemplatesView />
-                            </RmgErrorBoundary>
-                        }
-                    />
-                </Routes>
-            </RmgWindow>
+            <RmgThemeProvider>
+                <RmgWindow>
+                    <WindowHeader />
+                    <Routes>
+                        <Route
+                            path="/new"
+                            element={
+                                <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
+                                    <TicketView />
+                                </RmgErrorBoundary>
+                            }
+                        />
+                        <Route
+                            path="/"
+                            element={
+                                <RmgErrorBoundary suspenseFallback={<RmgLoader isIndeterminate />}>
+                                    <TemplatesView />
+                                </RmgErrorBoundary>
+                            }
+                        />
+                    </Routes>
+                </RmgWindow>
+            </RmgThemeProvider>
         </BrowserRouter>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import rmgRuntime from '@railmapgen/rmg-runtime';
-import { ChakraProvider } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import AppRoot from './components/app-root';
 import store from './redux';
 import i18n from './i18n/config';
 import { createRoot, Root } from 'react-dom/client';
-import { rmgChakraTheme } from '@railmapgen/rmg-components';
 import { I18nextProvider } from 'react-i18next';
 import initStore from './redux/init';
 import './index.css';
@@ -18,11 +16,9 @@ const renderApp = () => {
     root.render(
         <StrictMode>
             <Provider store={store}>
-                <ChakraProvider theme={rmgChakraTheme}>
-                    <I18nextProvider i18n={i18n}>
-                        <AppRoot />
-                    </I18nextProvider>
-                </ChakraProvider>
+                <I18nextProvider i18n={i18n}>
+                    <AppRoot />
+                </I18nextProvider>
             </Provider>
         </StrictMode>
     );
